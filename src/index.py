76984164a03abd6c6a7aed1a29970c14dd71aa6d
Rigoberto martinez <donojaci@gmail.com>
2022-02-22 13:50:47 -0600

    hay un error en el send del ctx

--- src/index.py
+++ src/index.py
@@ -4,12 +4,8 @@ import datetime
 bot = commands.Bot(command_prefix='>', description="this is a helper bot")
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-@bot.command()
 async def sum(ctx, NumOne: float, NumTwo: float):
-    await ctx.send(NumOne + NumTwo)
+    await  ctx.send(NumOne + NumTwo)
 
 @bot.command()
 async def info(ctx):
@@ -21,7 +17,6 @@ async def info(ctx):
 
 @bot.event
 async def on_ready():
-    # await bot.change_presence(activity=discord.Streaming(name="Admin"))
     print("Bot is ready ")
 
 bot.run('OTQ1NzQ3ODk5MzQ4ODE5OTg5.YhUqXg.TR5sRcr6bkTt4qRJ4RUwvAYQJ7w')
