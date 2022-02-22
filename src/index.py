1a5e4ec69b413958f1a6cd1ccd6b4239b64822bc
Rigoberto martinez <donojaci@gmail.com>
2022-02-22 12:54:12 -0600

    aplicnado operaciones matematicas

--- src/index.py
+++ src/index.py
@@ -7,11 +7,15 @@ bot = commands.Bot(command_prefix='>', description="this is a helper bot")
 async def ping(ctx):
     await ctx.send('pong')
 
+@bot.command()
+async def sum(ctx, NumOne: float, NumTwo: float):
+    await ctx.send(NumOne + NumTwo)
+
 #events
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity=discord.Streaming(name="Admin"))
+    # await bot.change_presence(activity=discord.Streaming(name="Admin"))
     print("Bot is ready ")
 
 bot.run('OTQ1NzQ3ODk5MzQ4ODE5OTg5.YhUqXg.TR5sRcr6bkTt4qRJ4RUwvAYQJ7w')
