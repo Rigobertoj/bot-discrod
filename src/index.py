d0d4c853de75f0b4b720fe37fad35c5a67884d05
Rigoberto martinez <donojaci@gmail.com>
2022-02-23 22:39:20 -0600

    seguimos con el error

--- src/index.py
+++ src/index.py
@@ -1,18 +1,41 @@
 import discord
 from discord.ext import commands
 import datetime
+
+from urllib import parse, request
+import re
+
 bot = commands.Bot(command_prefix='>', description="this is a helper bot")
 
 @bot.command()
-async def sum(ctx, NumOne: float, NumTwo: float):
-    await  ctx.send(NumOne + NumTwo)
+async def ping(ctx):
+    await ctx.send('pong')
+
+@bot.command()
+async def suma(ctx, OneNum: float, TwoNum: float):
+    await ctx.send(OneNum + TwoNum)
 
 @bot.command()
 async def info(ctx):
-    embed = discord.Embed(title=ctx, describe="lorem impsum"
-    , Timestamp=datetime.datetime.utcnow())
+    embed = discord.Embed(title=f"{ctx.guild.name}", description="Lorem Ipsum asdasd", timestamp=datetime.datetime.utcnow(), color=discord.Color.blue())
+    embed.add_field(name="Server created at", value=f"{ctx.guild.created_at}")
+    embed.add_field(name="Server Owner", value=f"{ctx.guild.owner}")
+    embed.add_field(name="Server Region", value=f"{ctx.guild.region}")
+    embed.add_field(name="Server ID", value=f"{ctx.guild.id}")
+    # embed.set_thumbnail(url=f"{ctx.guild.icon}")
+    embed.set_thumbnail(url="https://pluralsight.imgix.net/paths/python-7be70baaac.png")
+
     await ctx.send(embed=embed)
 
+@bot.command()
+async def youtube(ctx, *, search):
+    query_string = parse.urlencode({'search_query': search})
+    html_content = request.urlopen('http://www.youtube.com/results?' + query_string)
+    # print(html_content.read().decode())
+    search_results = re.findall('href=\"\\/watch\\?v=(.{11})', html_content.read().decode())
+    print(search_results)
+    # I will put just the first result, you can loop the response to show more results
+    await ctx.send('https://www.youtube.com/watch?v=' + search_results[0])
 #events
 
 @bot.event
